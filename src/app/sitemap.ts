3a651170ee17450b331d730c29521df9b5fb3ba1
Karan-Raj-KR <karanrajkr2008@gmail.com>
2026-08-02 11:14:15 +0530

    seo: add open-loop-2026 and project case studies to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,18 @@
 import { MetadataRoute } from 'next'
+import { projects } from '@/data/projects'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://www.karanrajkr.com'
-  const routes = ['', '/about', '/projects', '/blog', '/hackathons', '/achievements']
+  const routes = [
+    '',
+    '/about',
+    '/projects',
+    '/blog',
+    '/hackathons',
+    '/achievements',
+    '/hackathons/open-loop-2026',
+    ...projects.map((project) => `/projects/${project.slug}`),
+  ]
 
   return routes.map((route) => ({
     url: `${baseUrl}${route}`,
